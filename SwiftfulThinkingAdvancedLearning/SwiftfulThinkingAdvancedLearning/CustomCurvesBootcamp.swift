ed2f2787922f1c0c68699750e14b2b67313cf490
miguelmanasdev <miguel.manas.alvarez.dev@gmail.com>
2025-01-11 22:36:37 +0100

    Custom shapes with Arcs and Quad Curves in SwiftUI | Advanced Learning #6

---
+++ SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/CustomCurvesBootcamp.swift
@@ -0,0 +1,96 @@
+//
+//  CustomCurvesBootcamp.swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by Miguel Mañas Alvarez on 11/1/25.
+//
+
+import SwiftUI
+
+struct CustomCurvesBootcamp: View {
+    var body: some View {
+        WaterShape()
+            .frame(width: 200, height: 300)
+    }
+}
+
+struct CustomCurvesBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomCurvesBootcamp()
+    }
+}
+
+struct ArcSample: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.maxX, y: rect.midY))
+            path.addArc(
+                center: CGPoint(x: rect.midX, y: rect.midY),
+                radius: rect.height / 2,
+                startAngle: Angle(degrees: 0),
+                endAngle: Angle(degrees: 360),
+                clockwise: true)
+        }
+    }
+}
+
+struct ShapeWithArc: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            // top left
+            path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+            
+            // top right
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+            
+            // mid right
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+            
+            // bottom
+            path.addArc(
+                center: CGPoint(x: rect.midX, y: rect.midY),
+                radius: rect.height / 2,
+                startAngle: Angle(degrees: 0),
+                endAngle: Angle(degrees: 180),
+                clockwise: false)
+            //path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+            
+            // mid left
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.midY))
+        }
+    }
+    
+}
+
+struct QuadSample: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: .zero)
+            path.addQuadCurve(
+                to: CGPoint(x: rect.midX, y: rect.midY),
+                control: CGPoint(x: rect.maxX - 50, y: rect.minY - 100))
+        }
+    }
+}
+
+struct WaterShape: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.minX, y: rect.midY))
+            
+            path.addQuadCurve(
+                to: CGPoint(x: rect.midX, y: rect.midY),
+                control: CGPoint(x: rect.width * 0.25, y: rect.height * 0.40))
+            
+            path.addQuadCurve(
+                to: CGPoint(x: rect.maxX, y: rect.midY),
+                control: CGPoint(x: rect.width * 0.75, y: rect.height * 0.60))
+            
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        }
+    }
+}
