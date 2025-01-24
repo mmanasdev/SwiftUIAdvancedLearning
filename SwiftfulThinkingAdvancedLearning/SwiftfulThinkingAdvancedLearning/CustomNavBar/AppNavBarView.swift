fb10b6a3ad9ac69f7b3a03c7e3f53944ecb41672
miguelmanasdev <miguel.manas.alvarez.dev@gmail.com>
2025-01-24 18:07:56 +0100

    Create a custom navigation bar and link in SwiftUI | Advanced Learning #12 p1

---
+++ SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/CustomNavBar/AppNavBarView.swift
@@ -0,0 +1,20 @@
+//
+//  AppNavBarView.swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by Miguel Mañas Alvarez on 24/1/25.
+//
+
+import SwiftUI
+
+struct AppNavBarView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct AppNavBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppNavBarView()
+    }
+}
