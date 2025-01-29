205ef14f609eab51e874a78aadff92bd97c52ed0
miguelmanasdev <miguel.manas.alvarez.dev@gmail.com>
2025-01-29 23:05:46 +0100

    Use UIViewRepresentable to convert UIKit views to SwiftUI | Advanced Learning #13 P2

--- SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/UIViewRepresentableBootcamp.swift
+++ SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/UIViewRepresentableBootcamp.swift
@@ -14,8 +14,20 @@ struct UIViewRepresentableBootcamp: View {
     
     var body: some View {
         VStack {
-            Text("Hello, World!")
-            TextField("Type here...", text: $text)
+            Text(text)
+            HStack {
+                Text("SwiftUI: ")
+                TextField("Type here...", text: $text)
+                    .frame(height: 55)
+                    .background(Color.gray)
+            }
+            HStack {
+                Text("UIKit: ")
+                UITextFieldViewRepresentable(text: $text)
+                    .updatePlaceholder("NewPlaceholder!!")
+                    .frame(height: 55)
+                    .background(Color.gray)
+            }
         }
     }
 }
@@ -23,6 +35,67 @@ struct UIViewRepresentableBootcamp: View {
 struct UIViewRepresentableBootcamp_Previews: PreviewProvider {
     static var previews: some View {
         UIViewRepresentableBootcamp()
+    }
+}
+
+struct UITextFieldViewRepresentable: UIViewRepresentable {
+    
+    @Binding var text: String
+    var placeholder: String
+    let placeholderColor: UIColor
+    
+    init(text: Binding<String>,
+         placeholder: String = "Default placeholder...",
+         placeholderColor: UIColor = UIColor.red) {
+        self._text = text
+        self.placeholder = placeholder
+        self.placeholderColor = placeholderColor
+    }
+    
+    func makeUIView(context: Context) -> UITextField {
+        let textField = getTextField()
+        textField.delegate = context.coordinator
+        return textField
+    }
+    
+    //from Swiftui to UIKit
+    func updateUIView(_ uiView: UITextField, context: Context) {
+        uiView.text = text
+    }
+    
+    private func getTextField() -> UITextField {
+        let textField = UITextField(frame: .zero)
+        let placeholder = NSAttributedString(
+            string: placeholder,
+            attributes: [
+                .foregroundColor: placeholderColor
+        ])
+        textField.attributedPlaceholder = placeholder
+        return textField
+    }
+    
+    func updatePlaceholder(_ text: String) -> UITextFieldViewRepresentable {
+        var viewRepresentable = self
+        viewRepresentable.placeholder = text
+        return viewRepresentable
+    }
+    
+    //from UIKit to swiftUI
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(text: $text)
+    }
+    
+    class Coordinator: NSObject, UITextFieldDelegate {
+        
+        @Binding var text: String
+        
+        init(text: Binding<String>) {
+            self._text = text
+        }
+        
+        func textFieldDidChangeSelection(_ textField: UITextField) {
+            text = textField.text ?? ""
+        }
     }
 }
 
