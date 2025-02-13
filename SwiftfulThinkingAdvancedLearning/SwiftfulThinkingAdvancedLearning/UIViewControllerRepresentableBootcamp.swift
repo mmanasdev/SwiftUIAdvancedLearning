302a7e1a3f82c5abb5737510611f1b9dd8d36d95
miguelmanasdev <miguel.manas.alvarez.dev@gmail.com>
2025-02-13 18:03:46 +0100

    Use UIViewControllerRepresentable to convert UIKit controllers to SwiftUI | Advanced Learning #14 p2

--- SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/UIViewControllerRepresentableBootcamp.swift
+++ SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/UIViewControllerRepresentableBootcamp.swift
@@ -8,13 +8,19 @@
 import SwiftUI
 
 struct UIViewControllerRepresentableBootcamp: View {
+    
+    @State private var showSreen: Bool = false
+    
     var body: some View {
         VStack {
             Text("hi")
             Button {
-                
+                showSreen.toggle()
             } label: {
                 Text("Click here")
+            }
+            .sheet(isPresented: $showSreen) {
+                BasicUIViewControllerRepresentable(labelText: "New Text here")
             }
 
         }
@@ -28,9 +34,19 @@ struct UIViewControllerRepresentableBootcamp_Previews: PreviewProvider {
 }
 
 struct BasicUIViewControllerRepresentable: UIViewControllerRepresentable {
+    
+    let labelText: String
+    
     func makeUIViewController(context: Context) -> some UIViewController {
         let vc = UIViewController()
         vc.view.backgroundColor = .blue
+        
+        let label = UILabel()
+        label.text = "Hello, world"
+        label.textColor = UIColor.white
+        
+        vc.view.addSubview(label)
+        label.frame = vc.view.frame
         return vc
     }
     
