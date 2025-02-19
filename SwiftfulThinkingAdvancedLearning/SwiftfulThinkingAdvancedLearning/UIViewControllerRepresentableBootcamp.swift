c1a23f57d156cb2bd43336bbca3f0cd114cb0999
miguelmanasdev <miguel.manas.alvarez.dev@gmail.com>
2025-02-19 19:48:09 +0100

    Use UIViewControllerRepresentable to convert UIKit controllers to SwiftUI | Advanced Learning #14 p4

--- SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/UIViewControllerRepresentableBootcamp.swift
+++ SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/UIViewControllerRepresentableBootcamp.swift
@@ -10,17 +10,27 @@ import SwiftUI
 struct UIViewControllerRepresentableBootcamp: View {
     
     @State private var showSreen: Bool = false
+    @State private var image: UIImage? = nil
     
     var body: some View {
         VStack {
             Text("hi")
+            
+            if let image = image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+            
             Button {
                 showSreen.toggle()
             } label: {
                 Text("Click here")
             }
             .sheet(isPresented: $showSreen) {
-                BasicUIViewControllerRepresentable(labelText: "New Text here")
+                UIImagePickerControllerRepresentable(image: $image, showScreen: $showSreen)
+//                BasicUIViewControllerRepresentable(labelText: "New Text here")
             }
 
         }
@@ -30,6 +40,46 @@ struct UIViewControllerRepresentableBootcamp: View {
 struct UIViewControllerRepresentableBootcamp_Previews: PreviewProvider {
     static var previews: some View {
         UIViewControllerRepresentableBootcamp()
+    }
+}
+
+struct UIImagePickerControllerRepresentable: UIViewControllerRepresentable {
+    
+    @Binding var image: UIImage?
+    @Binding var showScreen: Bool
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let vc = UIImagePickerController()
+        vc.allowsEditing = false
+        vc.delegate = context.coordinator
+        return vc
+    }
+    
+    //from SwiftUI to UIKit
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+    
+    //from UIKit to SwiftUI
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(image: $image, showScreen: $showScreen)
+    }
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+        
+        @Binding var image: UIImage?
+        @Binding var showScreen: Bool
+        
+        init(image: Binding<UIImage?>, showScreen: Binding<Bool>) {
+            self._image = image
+            self._showScreen = showScreen
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            guard let newImage = info[.originalImage] as? UIImage else { return }
+            image = newImage
+            showScreen = false
+        }
     }
 }
 
